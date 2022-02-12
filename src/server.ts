6d00ba5ccd286a245046a40cf863f49635c8f0b7
Karsei <karseidev@gmail.com>
2022-02-12 15:16:31 +0900

    update: 인증 후 redirect 수정

--- src/server.ts
+++ src/server.ts
@@ -49,12 +49,12 @@ function init() {
             }
 
             await require('./services/NewsWebhookService').default.subscribe(params);
-            res.send(`<script>alert('봇이 추가되었습니다. 디스코드를 확인하세요.'); window.location.href = '/';</script>`);
+            res.send(`<script>alert('봇이 추가되었습니다. 디스코드를 확인하세요.'); window.location.href = '${Setting.DISCORD_URL_BOT_HOST}';</script>`);
         } catch (error) {
             Logger.err('봇을 추가하는 과정에서 오류가 발생했습니다.');
             Logger.err(error);
             console.log(error);
-            res.send(`<script>alert('봇을 추가하는 과정에서 오류가 발생했습니다.'); window.location.href = '/';</script>`);
+            res.send(`<script>alert('봇을 추가하는 과정에서 오류가 발생했습니다.'); window.location.href = '${Setting.DISCORD_URL_BOT_HOST}';</script>`);
         }
     });
 
