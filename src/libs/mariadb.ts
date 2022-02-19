879371d298be170fcfd374722b07402018672261
Karsei <karseidev@gmail.com>
2022-02-19 11:49:57 +0900

    update: maria db 연결 시 외부로 예외 던짐

--- src/libs/mariadb.ts
+++ src/libs/mariadb.ts
@@ -7,24 +7,19 @@ export default class MariaDbConnection {
     private static dbPool: any;
     private static dbCon: any;
     public static async init() {
-        try {
-            this.dbPool = mariadb.createPool({
-                host: Setting.MARIADB_HOST,
-                database: Setting.MARIADB_DATABASE,
-                user: Setting.MARIADB_USER,
-                password: Setting.MARIADB_PASSWORD,
-                port: Setting.MARIADB_PORT,
-                connectionLimit: Setting.MARIADB_CONNECTION_LIMIT,
-                connectTimeout: 5000,
-                acquireTimeout: 5000,
-                idleTimeout: 0,
-            });
+        this.dbPool = mariadb.createPool({
+            host: Setting.MARIADB_HOST,
+            database: Setting.MARIADB_DATABASE,
+            user: Setting.MARIADB_USER,
+            password: Setting.MARIADB_PASSWORD,
+            port: Setting.MARIADB_PORT,
+            connectionLimit: Setting.MARIADB_CONNECTION_LIMIT,
+            connectTimeout: 5000,
+            acquireTimeout: 5000,
+            idleTimeout: 0,
+        });
 
-            this.dbCon = await this.dbPool.getConnection();
-        }
-        catch (e) {
-            Logger.error('DB 연결 오류가 발생했습니다.', e);
-        }
+        this.dbCon = await this.dbPool.getConnection();
     }
 
     public static instance(): any {
