cc725408a2d79043610e4b26d2e8b490f86fdf46
Karsei <karseidev@gmail.com>
2022-02-26 18:35:05 +0900

    update: 패션체크에 한국어 구글 시트 추가

--- src/commands/fashion.ts
+++ src/commands/fashion.ts
@@ -31,6 +31,9 @@ module.exports = {
                 .setTitle(`패션 체크`)
                 .setDescription(`${latest.title}\n(Powered By. Kaiyoko Star)\n\n글로벌과 한국 서버의 패션 체크는 동일합니다.`)
                 .setTimestamp(new Date())
+                .setFields(
+                    { name: '한국어' , value: `[Google 시트](https://docs.google.com/spreadsheets/d/1RvbOnwLVlAKq7GwXwc3tAjQFtxQyk9PqrHa1J3vVB-g/edit#gid=174904573)` }
+                )
                 .setURL(`https://www.reddit.com/${latest.permalink}`)
                 .setImage(latest.url)
                 .setThumbnail('https://styles.redditmedia.com/t5_c3dzb/styles/profileIcon_ugxkdcpuxbp51.png?width=256&height=256&crop=256:256,smart&s=a1f754e55d562256c326bbc97302bc7d895e3806')
