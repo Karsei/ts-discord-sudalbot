206632311dc96de114c350957126b305bf11c12a
Karsei <karseidev@gmail.com>
2022-10-30 18:46:36 +0900

    del: deleted previous files

--- src/services/MarketService.ts
+++
@@ -1,8 +0,0 @@
-const axios = require('axios');
-
-export default class MarketService
-{
-    static async fetchCurrentList(pServer: string, pItemId: number) {
-        return await axios.get(`https://universalis.app/api/${pServer}/${pItemId}`);
-    }
-}
--- src/shared/lodestoneLocales.ts
+++
@@ -1,1 +0,0 @@
-export default [ 'na', 'jp', 'eu', 'de', 'fr' ];
