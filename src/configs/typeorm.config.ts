59ffe085ef257e387a8bfb5f5b6c537653ab38c9
Karsei <karseidev@gmail.com>
2022-10-30 22:18:23 +0900

    feat: added typeorm and mysql

---
+++ src/configs/typeorm.config.ts
@@ -0,0 +1,17 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+
+export const TypeORMConfig: TypeOrmModuleAsyncOptions = {
+    imports: [ ConfigModule ],
+    useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('MARIADB_HOST'),
+        port: configService.get('MARIADB_PORT'),
+        username: configService.get('MARIADB_USER'),
+        password: configService.get('MARIADB_PASSWORD'),
+        database: configService.get('MARIADB_DATABASE'),
+        entities: ['dist/**/*.entity.{ts,js}'],
+        synchronize: false,
+    }),
+    inject: [ ConfigService ],
+};
