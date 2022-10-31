8044af79d2b70153e86cbea1c985e593d03d23bc
Karsei <karseidev@gmail.com>
2022-10-31 23:34:09 +0900

    feat: added disabled tls option

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,9 @@ import {
 
 import { AppModule } from './app.module';
 
+// node 에서 허가되지 않은 인증 TLS 통신을 거부하지 않음
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 async function bootstrap() {
   const logger = WinstonModule.createLogger({
     transports: [
