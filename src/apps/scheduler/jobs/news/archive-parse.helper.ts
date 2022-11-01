601e1aeec6d55855907f689c320c53410300f89c
Karsei <karseidev@gmail.com>
2022-11-01 21:41:17 +0900

    refactor: changed type path

--- src/apps/scheduler/jobs/news/archive-parse.helper.ts
+++ src/apps/scheduler/jobs/news/archive-parse.helper.ts
@@ -1,5 +1,5 @@
 import { CheerioAPI, load as CheerioAPILoad } from 'cheerio';
-import { NewsContent } from './archive.constant';
+import { NewsContent } from '../../../../types/archive.constant';
 import { ArchiveFetchHelper } from './archive-fetch.helper';
 
 export class ArchiveParseHelper {
