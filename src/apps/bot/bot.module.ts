e6c4dcdcd61b2edd5870d0cfecbb5294e6053ae5
Karsei <karseidev@gmail.com>
2022-11-04 23:46:34 +0900

    feat: added contact command

--- src/apps/bot/bot.module.ts
+++ src/apps/bot/bot.module.ts
@@ -1,4 +1,5 @@
 import { Module, Logger } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { DiscordModule } from '@discord-nestjs/core';
 import { GatewayIntentBits } from 'discord.js';
 
@@ -8,19 +9,23 @@ import { EchoService } from './commands/echo/echo.service';
 import { EchoCommand } from './commands/echo/echo.command';
 import { UptimeService } from './commands/uptime/uptime.service';
 import { UptimeCommand } from './commands/uptime/uptime.command';
+import { ContactCommand } from './commands/contact/contact.command';
 import { FashionCheckService } from './commands/fashion/fashioncheck.service';
 import { FashionCheckCommand } from './commands/fashion/fashioncheck.command';
+import { Contact } from '../../entities/contact.entity';
 
 @Module({
   imports: [
     DiscordModule.forRootAsync(DiscordConfig),
     DiscordModule.forFeature(),
+    TypeOrmModule.forFeature([Contact]),
   ],
   providers: [
     Logger,
     BotGateway,
     EchoService, EchoCommand,
     UptimeService, UptimeCommand,
+    ContactCommand,
     FashionCheckService, FashionCheckCommand
   ]
 })
