e721c3af62db1e22c5afccd9a7a39d498e7a2612
Karsei <karseidev@gmail.com>
2022-11-06 12:43:12 +0900

    feat: added transform pipeline

--- src/apps/bot/commands/echo/echo.command.ts
+++ src/apps/bot/commands/echo/echo.command.ts
@@ -1,17 +1,20 @@
+import { TransformPipe } from '@discord-nestjs/common';
 import {
     Command,
     DiscordTransformedCommand,
     Payload,
     TransformedCommandExecutionContext,
+    UsePipes,
 } from '@discord-nestjs/core';
 
-import {EchoService} from './echo.service';
-import {EchoDto} from '../../dtos/echo.dto';
+import { EchoService } from './echo.service';
+import { EchoDto } from '../../dtos/echo.dto';
 
 @Command({
     name: '따라하기',
     description: '봇이 사용자의 말을 똑같이 따라합니다.',
 })
+@UsePipes(TransformPipe)
 export class EchoCommand implements DiscordTransformedCommand<EchoDto> {
     constructor(private readonly echoService: EchoService) {
     }
