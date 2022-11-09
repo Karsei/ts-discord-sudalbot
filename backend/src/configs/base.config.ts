98a2ae8ac88f685566814564747de920ced7eb26
Karsei <karseidev@gmail.com>
2022-11-09 23:13:34 +0900

    refactor: make a backend folder and changed paths

--- backend/src/configs/base.config.ts
+++ backend/src/configs/base.config.ts
@@ -1,5 +1,5 @@
 export const BaseConfig = {
     isGlobal: true,
-    envFilePath: `.env.${process.env.NODE_ENV}`,
+    envFilePath: `../.env.${process.env.NODE_ENV}`,
     ignoreEnvFile: 'prod' === process.env.NODE_ENV,
 };
