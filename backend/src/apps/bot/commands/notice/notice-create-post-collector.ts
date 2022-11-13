9fbe0bb335a72b2dea55a11e0ac3aa572667f74d
Karsei <karseidev@gmail.com>
2022-11-13 23:51:30 +0900

    feat: added managing news with db data

--- backend/src/apps/bot/commands/notice/notice-create-post-collector.ts
+++ backend/src/apps/bot/commands/notice/notice-create-post-collector.ts
@@ -1,14 +1,19 @@
 import { Message, PermissionsBitField, SelectMenuInteraction } from 'discord.js';
+import { Repository } from 'typeorm';
 import Redis from 'ioredis';
+import { RedisService } from '@liaoliaots/nestjs-redis';
+import { InjectRepository } from '@nestjs/typeorm';
 import { Inject, Logger, LoggerService } from '@nestjs/common';
 import { InteractionEventCollector, On, Once } from '@discord-nestjs/core';
-import { RedisService } from '@liaoliaots/nestjs-redis';
+
+import { News } from '../../../../entities/news.entity';
 
 @InteractionEventCollector({ time: 15000 })
 export class NoticeCreatePostCollector {
     private readonly redis: Redis;
     constructor(@Inject(Logger) private readonly loggerService: LoggerService,
-                private readonly redisService: RedisService) {
+                private readonly redisService: RedisService,
+                @InjectRepository(News) private newsRepository: Repository<News>) {
         this.redis = this.redisService.getClient();
     }
 
@@ -28,7 +33,7 @@ export class NoticeCreatePostCollector {
             const values = interaction.values[0].split("||");
             const locale = values[0], topic = values[1];
 
-            await this.addUrl(locale, topic, hookUrl);
+            await this.addUrl(interaction.guildId, locale, topic, hookUrl);
             this.loggerService.log(`${interaction.guild} (${interaction.guildId}) - 언어: ${locale}, 카테고리: ${topic} - 소식을 추가하였습니다.`);
             await interaction.editReply({content: '소식 추가에 성공했어요!', components: []});
         }
@@ -58,11 +63,18 @@ export class NoticeCreatePostCollector {
     /**
      * 게시글별 Webhook URL Cache 등록
      *
+     * @param guildId 서버 ID
      * @param locale 언어
      * @param type 카테고리
      * @param url Webhook URL
      */
-    private async addUrl(locale: string, type: string, url: string) {
+    private async addUrl(guildId: string, locale: string, type: string, url: string) {
+        await this.newsRepository.insert({
+            guild: { id: guildId },
+            locale: locale,
+            type: type,
+            url: url,
+        });
         return this.redis.sadd(`${locale}-${type}-webhooks`, url);
     }
 
