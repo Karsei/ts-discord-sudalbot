011efecaa92cec2bcead319d0078cac837af0e49
Karsei <karseidev@gmail.com>
2023-01-13 21:17:12 +0900

    refactor: temporarily modified for managing duplicate logs within the class

--- backend/src/apps/scheduler/jobs/news/archive.service.ts
+++ backend/src/apps/scheduler/jobs/news/archive.service.ts
@@ -1,10 +1,11 @@
+import { AxiosError } from 'axios';
 import { Inject, Injectable, Logger, LoggerService } from '@nestjs/common';
 import { RedisService } from '@liaoliaots/nestjs-redis';
 import Redis from 'ioredis';
 
-import NewsCategories, { NewsCategoryGlobal, NewsCategoryKorea, NewsContent } from '../../../../definitions/archive.constant';
-import GlobalErrorReport from '../../../../helpers/global-error-report.helper';
 import { ArchiveFetchHelper } from './archive-fetch.helper';
+import NewsCategories, { NewsCategoryGlobal, NewsCategoryKorea, NewsContent } from '../../../../definitions/archive.constant';
+import GlobalErrorReport, {ErrorLevel} from '../../../../helpers/global-error-report.helper';
 
 @Injectable()
 export class ArchiveService {
@@ -34,8 +35,7 @@ export class ArchiveService {
                 await this.setCache(JSON.stringify(data), type, locale);
                 return data;
             } catch (e) {
-                GlobalErrorReport.report('warn', '글로벌 소식을 가져오는 과정에서 오류가 발생했습니다.');
-                this.loggerService.warn('글로벌 소식을 가져오는 과정에서 오류가 발생했습니다.', e);
+                this.logMessage('warn', '글로벌 소식을 가져오는 과정에서 오류가 발생했습니다.', e);
                 let data = await this.getCache(type, locale);
                 return JSON.parse(data);
             }
@@ -71,8 +71,7 @@ export class ArchiveService {
                 await this.setCache(JSON.stringify(data), type, 'kr');
                 return data;
             } catch (e) {
-                GlobalErrorReport.report('warn', '한국 소식을 가져오는 과정에서 오류가 발생했습니다.');
-                this.loggerService.warn('한국 소식을 가져오는 과정에서 오류가 발생했습니다.', e);
+                this.logMessage('warn', '한국 소식을 가져오는 과정에서 오류가 발생했습니다.', e);
                 let data = await this.getCache(type, 'kr');
                 return JSON.parse(data);
             }
@@ -123,5 +122,13 @@ export class ArchiveService {
         let timestamp = await this.redis.hget(`${locale}-news-timestamp`, type);
         let cacheTime = timestamp ? parseInt(timestamp) : new Date(0).getTime();
         return new Date().getTime() > cacheTime + ArchiveService.CACHE_EXPIRE_IN;
+    }
+
+    private async logMessage(level: ErrorLevel, title: string, error: Error) {
+        if (error instanceof AxiosError) {
+            GlobalErrorReport.report(level, title, error.message, error.stack);
+        }
+        if (level === 'warn') this.loggerService.warn(title, error);
+        else if (level === 'error') this.loggerService.error(title, error);
     }
 }
