e9b798404b26fb65c6093c5b3c66732b9b67ef4c
Karsei <karseidev@gmail.com>
2023-01-13 13:23:47 +0900

    chore: fixed timezone

--- backend/src/configs/typeorm.config.ts
+++ backend/src/configs/typeorm.config.ts
@@ -7,10 +7,12 @@ import { Contact } from '../entities/contact.entity';
 import { XivVersion } from '../entities/xiv-version.entity';
 import { XivItem } from '../entities/xiv-item.entity';
 import { XivItemCategories } from '../entities/xiv-item-categories.entity';
+import { Chat } from "../entities/chat.entity";
 
 export const TypeORMConfig: TypeOrmModuleAsyncOptions = {
     imports: [ ConfigModule ],
     useFactory: (configService: ConfigService) => ({
+        timezone: 'Asia/Seoul',
         type: 'mysql',
         host: configService.get('MARIADB_HOST'),
         port: configService.get('MARIADB_PORT'),
@@ -18,7 +20,7 @@ export const TypeORMConfig: TypeOrmModuleAsyncOptions = {
         password: configService.get('MARIADB_PASSWORD'),
         database: configService.get('MARIADB_DATABASE'),
         //entities: ['../**/*.entity.{ts,js}'],
-        entities: [Guild, News, Contact, XivVersion, XivItem, XivItemCategories],
+        entities: [Guild, News, Contact, XivVersion, XivItem, XivItemCategories, Chat],
         synchronize: 'prod' !== process.env.NODE_ENV,
     }),
     inject: [ ConfigService ],
