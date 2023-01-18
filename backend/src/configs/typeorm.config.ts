ee5c1076c3d652d8b79373482cfed8d544414d09
Karsei <karseidev@gmail.com>
2023-01-18 21:34:03 +0900

    del: removed unnecessary codes

--- backend/src/configs/typeorm.config.ts
+++ backend/src/configs/typeorm.config.ts
@@ -1,14 +1,6 @@
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
 
-import { News } from '../entities/news.entity';
-import { Guild } from '../entities/guild.entity';
-import { Contact } from '../entities/contact.entity';
-import { XivVersion } from '../entities/xiv-version.entity';
-import { XivItem } from '../entities/xiv-item.entity';
-import { XivItemCategories } from '../entities/xiv-item-categories.entity';
-import { Chat } from "../entities/chat.entity";
-
 export const TypeORMConfig: TypeOrmModuleAsyncOptions = {
     imports: [ ConfigModule ],
     useFactory: (configService: ConfigService) => ({
@@ -19,8 +11,7 @@ export const TypeORMConfig: TypeOrmModuleAsyncOptions = {
         username: configService.get('MARIADB_USER'),
         password: configService.get('MARIADB_PASSWORD'),
         database: configService.get('MARIADB_DATABASE'),
-        //entities: ['../**/*.entity.{ts,js}'],
-        entities: [Guild, News, Contact, XivVersion, XivItem, XivItemCategories, Chat],
+        entities: ['dist/**/*.entity.{ts,js}'],
         synchronize: 'prod' !== process.env.NODE_ENV,
     }),
     inject: [ ConfigService ],
