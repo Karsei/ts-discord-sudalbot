041f6cd71721e83dc234b12c356cf8fa0bad18a3
Karsei <karseidev@gmail.com>
2023-01-27 14:07:38 +0900

    feat: fixed selecting item from list

--- backend/src/apps/bot/bot.module.ts
+++ backend/src/apps/bot/bot.module.ts
@@ -12,16 +12,16 @@ import { UptimeCommand } from './commands/uptime/uptime.command';
 import { ContactCommand } from './commands/contact/contact.command';
 import { FashionCheckService } from './commands/fashion/fashioncheck.service';
 import { FashionCheckCommand } from './commands/fashion/fashioncheck.command';
-import { ItemSearchService} from './commands/item/item-search.service';
-import { ItemSearchCommand} from './commands/item/item-search.command';
+import { ItemSearchService } from './commands/item/item-search.service';
+import { ItemSearchCommand } from './commands/item/item-search.command';
 import { XivapiService } from './commands/item/xivapi.service';
 import { Contact } from '../../entities/contact.entity';
 import { XivVersion } from '../../entities/xiv-version.entity';
 import { XivItem } from '../../entities/xiv-item.entity';
-import { News } from "../../entities/news.entity";
-import { Chat } from "../../entities/chat.entity";
+import { News } from '../../entities/news.entity';
+import { Chat } from '../../entities/chat.entity';
 import { XivItemCategories } from '../../entities/xiv-item-categories.entity';
-import { MarketService} from './commands/market/market.service';
+import { MarketService } from './commands/market/market.service';
 import { MarketCommand } from './commands/market/market.command';
 import { UniversalisService } from './commands/market/universalis.service';
 import { NoticeService } from './commands/notice/notice.service';
@@ -29,24 +29,44 @@ import { NoticeCreateCommand } from './commands/notice/notice-create.command';
 import { NoticeDeleteCommand } from './commands/notice/notice-delete.command';
 import { ChatGptCommand } from './commands/openai/chatgpt.command';
 import { ChatGptService } from './commands/openai/chatgpt.service';
+import { ItemSearchInteractionService } from './commands/item/item-search-interaction.service';
 
 @Module({
   imports: [
     DiscordModule.forRootAsync(DiscordConfig),
     DiscordModule.forFeature(),
-    TypeOrmModule.forFeature([Contact, XivVersion, XivItem, XivItemCategories, News, Chat]),
+    TypeOrmModule.forFeature([
+      Contact,
+      XivVersion,
+      XivItem,
+      XivItemCategories,
+      News,
+      Chat,
+    ]),
   ],
   providers: [
     Logger,
     BotGateway,
-    EchoService, EchoCommand,
-    UptimeService, UptimeCommand,
+    EchoService,
+    EchoCommand,
+    UptimeService,
+    UptimeCommand,
     ContactCommand,
-    FashionCheckService, FashionCheckCommand,
-    ItemSearchService, ItemSearchCommand, XivapiService,
-    MarketService, MarketCommand, MarketService, UniversalisService,
-    NoticeService, NoticeCreateCommand, NoticeDeleteCommand,
-    ChatGptService, ChatGptCommand,
-  ]
+    FashionCheckService,
+    FashionCheckCommand,
+    ItemSearchService,
+    ItemSearchCommand,
+    ItemSearchInteractionService,
+    XivapiService,
+    MarketService,
+    MarketCommand,
+    MarketService,
+    UniversalisService,
+    NoticeService,
+    NoticeCreateCommand,
+    NoticeDeleteCommand,
+    ChatGptService,
+    ChatGptCommand,
+  ],
 })
 export class BotModule {}
