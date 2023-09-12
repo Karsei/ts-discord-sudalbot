174cf38df50021b177d8dd378e62487d153f5568
Karsei <karseidev@gmail.com>
2023-09-12 23:52:22 +0900

    build: pm2 설정 수정

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,17 @@
-// 윈도우는 https://github.com/Unitech/pm2/issues/3657#issuecomment-482010714 참고
 module.exports = {
   apps: [
     {
       name: 'daldalee-bot',
+      // script: 'C:\\Program Files\\nodejs\\node_modules\\npm\\bin\\npm-cli.js', // windows - https://github.com/Unitech/pm2/issues/3657#issuecomment-482010714
       script: 'npm',
-      args: 'run start',
+      args: 'run start:env',
+      autorestart: true,
+      watch: false,
+      instance: '1',
       env: {
         NODE_ENV: "dev",
       },
-      env_prod: {
+      env_prod: { // --env prod
         NODE_ENV: "prod"
       },
     },
