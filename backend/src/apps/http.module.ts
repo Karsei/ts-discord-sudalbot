8eecb6b3f9da3c1332cbf4b5bf7877fdc920c39c
Karsei <karseidev@gmail.com>
2023-12-22 22:49:37 +0900

    fix: 누락된 의존 주입 추가

--- backend/src/apps/http.module.ts
+++ backend/src/apps/http.module.ts
@@ -8,10 +8,17 @@ import { WebhookController } from './adapter/in/http/webhook.controller';
 import { WebhookService } from './service/webhook/webhook.service';
 import { Guild } from '../entities/guild.entity';
 import { News } from '../entities/news.entity';
+import { RedisAdapter } from './adapter/out/redis.adapter';
+import { NewsPublishCacheLoadPortToken } from './port/out/news-publish-cache-load-port.interface';
 
 @Module({
   imports: [TerminusModule, TypeOrmModule.forFeature([Guild, News])],
   controllers: [HealthController, WebhookController],
-  providers: [Logger, SampleHealthIndicator, WebhookService],
+  providers: [
+    Logger,
+    SampleHealthIndicator,
+    WebhookService,
+    { provide: NewsPublishCacheLoadPortToken, useClass: RedisAdapter },
+  ],
 })
 export class HttpModule {}
